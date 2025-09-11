92061e64f937ea66f452635b6fcb2ad6ccae4e92
nico-mauber <nicomauber1@gmail.com>
2025-09-11 15:46:47 -0300

    versiones

--- uruguahorra-app/babel.config.js
+++ uruguahorra-app/babel.config.js
@@ -1,22 +1,3 @@
 module.exports = {
   presets: ['babel-preset-expo'],
-  plugins: [
-    'react-native-reanimated/plugin',
-    // Plugin personalizado para transformar import.meta
-    './babel-import-meta-transform.js',
-    [
-      'module-resolver',
-      {
-        root: ['./'],
-        alias: {
-          '@': './src',
-          '@components': './src/components',
-          '@features': './src/features',
-          '@lib': './src/lib',
-          '@store': './src/store',
-          '@theme': './src/theme',
-        },
-      },
-    ],
-  ],
 };
--- uruguahorra-app/metro.config.js
+++ uruguahorra-app/metro.config.js
@@ -1,8 +1,3 @@
 const { getDefaultConfig } = require('expo/metro-config');
 
-const config = getDefaultConfig(__dirname);
-
-// Add web platform support
-config.resolver.platforms = ['web', 'native', 'ios', 'android'];
-
-module.exports = config;
+module.exports = getDefaultConfig(__dirname);
